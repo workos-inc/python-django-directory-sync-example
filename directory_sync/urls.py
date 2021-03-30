4c48b6e65595791f683ce33e3d10369901c2282e
erin@workos.com <erin@workos.com>
2021-03-30 15:19:39 -0700

    Remove leading slash from URLs

--- directory_sync/urls.py
+++ directory_sync/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/users', views.directory_users, name='users'),
-    path('/groups', views.directory_groups, name='groups'),
+    path('users', views.get_directory_users, name='users'),
+    path('groups', views.get_directory_groups, name='groups'),
 ]
