1c120b466c8a75c033311bee0a34aa8a436c4df5
erin@workos.com <erin@workos.com>
2021-03-30 17:23:25 -0700

    Remove print statement

--- directory_sync/views.py
+++ directory_sync/views.py
@@ -14,7 +14,6 @@ directory_id = os.getenv('DIRECTORY_ID')  # Follow the WorkOS guide to get this
 
 def get_directory_users(request):
     users = workos.client.directory_sync.list_users(directory=directory_id)
-    print(users)
     return JsonResponse(data=users)
 
 
