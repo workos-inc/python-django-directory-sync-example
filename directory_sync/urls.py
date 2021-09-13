752ee3c77f46d869ca2946c36c5930b86906aed4
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2021-09-13 16:37:46 -0600

    updated the app and readme with the new branding and associated changes

--- directory_sync/urls.py
+++ directory_sync/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.get_home, name='home'),
     path('users', views.get_directory_users, name='users'),
     path('groups', views.get_directory_groups, name='groups'),
 ]
